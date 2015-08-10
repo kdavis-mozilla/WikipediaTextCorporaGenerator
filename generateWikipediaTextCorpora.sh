905f9ee4e45e7255e2fc172237610f1ee477e626
Kelly Davis <kdavis@mozilla.com>
2015-08-10 16:37:49 +0200

    Adjected to new location of scripts

--- generateWikipediaTextCorpora.sh
+++ generateWikipediaTextCorpora.sh
@@ -2,8 +2,8 @@
 NC='\033[0m'
 RED='\033[0;31m'
 echo -e "${RED}Making the directories to store the Wikipedia text corpora${NC}"
-./makeDirectories.sh
+bin/makeDirectories.sh
 echo -e "${RED}Downloading the Wikipedia dumps${NC}"
-./downloadWikipediaDumps.sh
+bin/downloadWikipediaDumps.sh
 echo -e "${RED}Extracting the raw text from the Wikipedia dumps${NC}"
-./extractTextOfWikipedias.sh
+bin/extractTextOfWikipedias.sh
